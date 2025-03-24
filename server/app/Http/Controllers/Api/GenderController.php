d0d6c6163381991ace7120c67216c3adce789803
stephaniehanna <bacarrohanna8@gmail.com>
2025-03-25 01:04:23 +0800

    Added services, routes,and api for Gender

--- server/app/Http/Controllers/Api/GenderController.php
+++ server/app/Http/Controllers/Api/GenderController.php
@@ -7,14 +7,17 @@ use Illuminate\Http\Request;
 
 class GenderController extends Controller
 {
-    public function StoreGender(Request $request) {
+    public function storeGender(Request $request) {
         $validated = $request->validate([
             'gender' => ['required', 'min:4', 'max:10']
         ]);
 
         Gender::create([
-            ''
+            'gender' => $validated['gender']
         ]);
 
+        return response() ->json([
+            'message' => 'Gender Successfully Added.'
+        ], 200);
     }
 }
